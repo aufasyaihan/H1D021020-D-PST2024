89653e7513876d70d72df142f57ea3d564de2d7e
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-05-07 11:18:27 +0700

    Pertemuan 5

---
+++ LCD/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 15, 1); // (address, columns, rows)
+int SDA_pin = D4;
+int SCL_pin = D3;
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  Wire.begin(SDA_pin, SCL_pin);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Halo nama saya gatau ini apaan si gajelas jir");
+}
+
+void loop() {
+  lcd.scrollDisplayLeft();
+  delay(300);
+}
